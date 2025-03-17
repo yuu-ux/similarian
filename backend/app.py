9a3e540ffb5367b4952329ce20c2807f0b016a5b
yuu-ux <yuugaehara@gmail.com>
2025-03-17 23:10:21 +0900

    :wrench: dotenv 追加

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from opensearchpy import OpenSearch
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
 # OpenSearch の接続設定
